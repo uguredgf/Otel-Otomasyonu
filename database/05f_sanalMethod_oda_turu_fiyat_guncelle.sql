d199a87686eadaaf5f7728f9ccd32ca5e0e0d566
uguredgf <ugurtlf23@gmail.com>
2026-05-17 23:22:01 +0300

    Fiyat güncelleme kısmı tamam

--- database/05f_sanalMethod_oda_turu_fiyat_guncelle.sql
+++ database/05f_sanalMethod_oda_turu_fiyat_guncelle.sql
@@ -1,16 +1,19 @@
--- Oda türlerinin taban fiyatlarını dinamik olarak güncellemek için oluşturulan prosedür
-DROP PROCEDURE IF EXISTS sp_OdaTuruFiyatGuncelle;
+-- Oda türlerinin taban fiyatlarını ve kapasitesini isimlerine göre dinamik olarak güncellemek için oluşturulan prosedür
+DROP PROCEDURE IF EXISTS sp_OdaTuruGuncelle;
 DELIMITER //
 
-CREATE PROCEDURE sp_OdaTuruFiyatGuncelle(
-    IN p_odaTur_id INT,
-    IN p_yeni_fiyat DECIMAL(10,2)
+CREATE PROCEDURE sp_OdaTuruGuncelle(
+    IN p_oda_turu_adi VARCHAR(100),
+    IN p_yeni_fiyat DECIMAL(10,2),
+    IN p_yeni_kapasite INT
 )
 BEGIN
-    -- İlgili oda türünün taban fiyatını yeni fiyat ile değiştiriyoruz
+    -- Oda türünün hem fiyatını hem de kapasitesini ismine göre güncelliyoruz
     UPDATE Oda_Turleri 
-    SET odaTur_taban_fiyat = p_yeni_fiyat
-    WHERE odaTur_id = p_odaTur_id;
+    SET 
+        odaTur_taban_fiyat = p_yeni_fiyat,
+        odaTur_kapasite = p_yeni_kapasite
+    WHERE odaTur_adi = p_oda_turu_adi;
     
 END //
 
